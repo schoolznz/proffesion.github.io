e2492ef65171942754e22712f339be36c01f0e54
schoolznz <59060862+schoolznz@users.noreply.github.com>
2019-12-19 18:18:18 +0200

    Add files via upload

---
+++ site/news.js
@@ -0,0 +1,24 @@
+	 
+	  var elemIDmain = document.getElementById('main');
+	  	 
+	  
+	 	   
+	  function change() {
+		  if ( elemIDmain.style.color == 'black') {
+	  
+	  	  elemIDmain.style.fontFamily = 'Georgia'; 
+	      
+	  	  elemIDmain.style.fontSize = '16px';
+	  	  elemIDmain.style.color = 'red';
+	  
+       } else 
+		   
+		   {
+			 elemIDmain.style.fontFamily = 'verdana'; 
+	     
+	  	  elemIDmain.style.fontSize = '12px';
+	  	  elemIDmain.style.color = 'black';
+		   }
+	  }
+ 
+	
